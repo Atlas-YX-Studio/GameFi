7b3fca0659eeb81a5d405c6d95817e4b54a73612
zhangcheng <472012214@qq.com>
2021-12-10 17:46:45 +0800

    删除 注释

--- src/main/java/com/bixin/gameFi/aww/bean/dto/TakeOrderEventDto.java
+++ src/main/java/com/bixin/gameFi/aww/bean/dto/TakeOrderEventDto.java
@@ -49,31 +49,3 @@ public class TakeOrderEventDto {
 
 }
 
-/*
-
-    private Long id;
-
-    private String sellAddress;
-
-    private String buyAddress;
-
-    private Long awwId;
-
-    private Long groupId;
-
-    private String icon;
-
-    private String groupName;
-
-    private String awwName;
-
-    private BigDecimal price;
-
-    private BigDecimal fee;
-
-    private Long matchTime;
-
-    private Long createTime;
-
-    private Long updateTime;
-*/
