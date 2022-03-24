f1111b44dd88957d27f5052ec0ae15a2789772b8
ray0625 <jasonren1990@163.com>
2022-03-24 10:58:52 +0800

    修改合约版本

--- src/main/java/com/bixin/gameFi/aww/service/Impl/BOBMarketImpl.java
+++ src/main/java/com/bixin/gameFi/aww/service/Impl/BOBMarketImpl.java
@@ -46,9 +46,9 @@ public class BOBMarketImpl implements IBOBMarketService {
     BobMintInfoMapper bobMintInfoMapper;
 
     private static final String separator = "::";
-    private static final String bobSuffix = separator + "BOBConfigV5" + separator + "Config";
-    private static final String bobSuffix_NormalTicket = "BOBNormalTicketV5";
-    private static final String bobSuffix_NormalRace = "BOBNormalRaceV5";
+    private static final String bobSuffix = separator + "BOBConfigV6" + separator + "Config";
+    private static final String bobSuffix_NormalTicket = "BOBNormalTicketV7";
+    private static final String bobSuffix_NormalRace = "BOBNormalRaceV8";
     private static final String bobSuffix_NormalRaceInfo = separator + bobSuffix_NormalRace + separator + "RaceInfo";
 
 //0x00000000000000000000000000000001::NFTGallery::NFTGallery<0x9b996121ea29b50c6213558e34120e5c::BOBNormalTicketV3::Meta, 0x9b996121ea29b50c6213558e34120e5c::BOBNormalTicketV3::Body>
